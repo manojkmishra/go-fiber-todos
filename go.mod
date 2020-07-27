c6c77230a68c13069aea8b7717857d3a8a11d589
manojkmishra <manojmishra571980@gmail.com>
2020-07-27 23:15:01 +1000

    server running on port 3000

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module go-fiber-todos
 go 1.14
 
 require (
-	github.com/gofiber/fiber v1.13.3 // indirect
+	github.com/gofiber/fiber v1.13.3
 	github.com/klauspost/compress v1.10.10 // indirect
 	golang.org/x/sys v0.0.0-20200724161237-0e2f3a69832c // indirect
 )
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+import "github.com/gofiber/fiber"
+type Todo struct {
+	Id int `json:"id"`
+	Name string `json:"name"`
+	Completed bool `json:"completed"`
+}
+var todos=[]Todo{
+	{Id:1,Name:"first one",Completed:false},
+	{Id:2,Name:"first one",Completed:false},
+}
+func main(){
+	app := fiber.New()
+	app.Get( "/" , func(ctx *fiber.Ctx){ ctx.Send("hello world")	})
+	err:=app.Listen(3000)
+	if err != nil { panic(err) }
+}
